5a5cfd71258fbdbe9b4425f0395c0328caec8d0f
pasi0014 <pasi0014@algonquinlive.com>
2020-11-10 15:50:32 -0700

    Add project files.

---
+++ lab4/Account.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public class Account
+    {
+        private int _acNumber;
+        private double _balance;
+        private string _ownName;
+        private double _monthlyDepositAmount;
+        
+
+
+        public static double MonthlyFee = 4.0;
+        public static double MonthlyInterestRate = 0.0025;
+        public static int MinimumInitialBalance = 1000;
+        public static int MinimumMonthDeposit = 50;
+
+
+        public  int AccountNumber 
+        {
+            get {
+                return this._acNumber;
+            }
+            set {
+                this._acNumber = value;
+            }
+        }
+
+        
+        public string AccountOwner
+        {
+            get
+            {
+                return _ownName;
+            }
+            set
+            {
+                this._ownName = value;
+            }
+        }
+
+        public double AccountBalance
+        {
+            get
+            {
+                return this._balance;
+            }
+            set
+            {
+                this._balance = value;
+            }
+        }
+
+        public double MonthlyDepositAmount
+        {
+            get
+            {
+                return _monthlyDepositAmount;
+            }
+            set
+            {
+                _monthlyDepositAmount = value;
+            }
+        }
+
+        public void Deposit(double MonthlyDepositAmount)
+        {
+            if (MonthlyDepositAmount < MinimumMonthDeposit)
+            {
+                AccountBalance += MinimumMonthDeposit;
+            }
+           AccountBalance +=( MonthlyDepositAmount + AccountBalance * MonthlyInterestRate);
+
+        }
+
+        public void Withdraw(double MonthlyDepositAmount)
+        {
+            if (AccountBalance >= MonthlyDepositAmount)
+            {
+                AccountBalance -= MonthlyDepositAmount;
+            }  
+        }
+        public double Deduction()
+        {
+            //Deduction Fee
+            AccountBalance -= MonthlyFee;
+            return AccountBalance;
+        }
+        public Account(string accountOwner, double accountBalance)
+        {
+            _ownName = accountOwner;
+            _balance = accountBalance;
+            if (AccountBalance<MinimumInitialBalance)
+            {
+                AccountBalance = MinimumInitialBalance;
+            }
+            //Generate random users account number
+            Random random = new Random();
+            _acNumber = random.Next(90000, 99999);
+        }
+
+
+
+
+    }
+}
---
+++ lab4/Bank.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class Bank
+    {
+        
+
+        static void Main(string[] args)
+        {
+            //Initialize variables 
+            string name;
+            string initial;
+            double initialDeposit;
+            double monthDeposit;
+            string monthsDeposit;
+
+            Console.Write("Enter the number of month to deposit: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n");
+
+            Account account = null;
+            List < Account > accountList = new List<Account>();
+
+
+
+            while (true)
+            {
+                Console.Write("Enter Customer Name: ");
+                name = Console.ReadLine();
+
+                if (name == "")
+                {
+                    Console.WriteLine("\n\n");
+                    break;
+                }
+
+                Console.Write("Enter " + name + "'s" + " Initial Deposit Amount (minimum $1,000.00): ");
+                initial = Console.ReadLine();
+                initialDeposit = Convert.ToDouble(initial);
+
+                Console.Write("Enter " + name + "'s " + "Montly Deposit Amount (minimum $50.00): ");
+                monthsDeposit = Console.ReadLine();
+                monthDeposit = Convert.ToDouble(monthsDeposit);
+
+                Console.WriteLine("\n");
+
+                account = new Account(name, initialDeposit);
+
+                for (int i = 0; i < months; i++)
+                {
+                    account.Deposit(monthDeposit);
+                }
+
+
+                accountList.Add(account);
+
+            }
+
+
+
+            foreach (var n in accountList)
+            {
+                n.Deduction();
+                Console.Write("After " + months + " months " + n.AccountOwner + "'s account (#" + n.AccountNumber + "), has a balance of: $" + n.AccountBalance + "\n"); 
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Press Enter to complete");
+
+            Console.Read();
+        }
+    }
+}
